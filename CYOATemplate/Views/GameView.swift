5660e5db2cba487c510c63d81512d4637679d003
Jerry Dai <jdai@lcs.on.ca>
2023-06-05 15:21:14 -0400

    added the button to switch between light mode and dark mode.

--- CYOATemplate/Views/GameView.swift
+++ CYOATemplate/Views/GameView.swift
@@ -13,6 +13,9 @@ struct GameView: View {
     // MARK: Stored properties
     @State var currentNodeId: Int = 1
     
+    @AppStorage("isDarkMode") private var isDarkMode:Bool = false
+    @Environment(\.presentationMode) var presentationMode
+    
     // MARK: Computed properties
     var body: some View {
         VStack(spacing: 10) {
@@ -24,16 +27,49 @@ struct GameView: View {
             }
             
             NodeView(currentNodeId: currentNodeId)
-            
             Divider()
             
             EdgesView(currentNodeId: $currentNodeId)
-                        
             Spacer()
+            
+            HStack{
+                Button(action: {}, label: {
+                    Image(systemName: "textformat")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 30,height: 20)
+                        .foregroundColor(.black)
+                })
+                
+                
+                Spacer()
+                Button(action: {}, label: {
+                    Image(systemName: "light.max")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 30,height: 20)
+                        .foregroundColor(.black)
+                })
+                Spacer()
+                Button(action: {
+                    isDarkMode.toggle()
+                }, label: {
+                    Image(systemName: "moon.zzz")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 30,height: 20)
+                        .foregroundColor(.black)
+                })
+            }
+            .frame(height: UIScreen.main.bounds.height / 10)
+            .frame(maxWidth: .infinity)
+            .background(Color(.systemGray5))
             
         }
         .padding()
+        .preferredColorScheme(isDarkMode ? .dark:.light)
     }
+        
 }
 
 struct GameView_Previews: PreviewProvider {

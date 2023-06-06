361b148a730899abe212436785d9fc068d8a873f
Jerry Dai <jdai@lcs.on.ca>
2023-06-06 14:52:50 -0400

    The slider bar for the brightness button is now working properly

---
+++ CYOATemplate/Extensions/PopUpMenu.swift
@@ -0,0 +1,37 @@
+//
+//  PopUpMenu.swift
+//  CYOATemplate
+//
+//  Created by Dai Jiaze on 2023-06-05.
+//
+
+//import SwiftUI
+//import UIKit
+//
+//struct PopUpMenu: View {
+//    var body: some View {
+//        VStack{
+//            Spacer()
+//            MenuItem()
+//            Spacer()
+//        }
+//        .transition(.scale)
+//    }
+//}
+//
+//struct MenuItem: View{
+//    @State private var brightness: Double = 1
+//    var body: some View{
+//        VStack{
+//            Slider(value: $brightness, in: 0...1)
+//        }
+//        .transition(.scale)
+//
+//    }
+//}
+//
+//struct PopUpMenu_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PopUpMenu()
+//    }
+//}
--- CYOATemplate/Views/GameView.swift
+++ CYOATemplate/Views/GameView.swift
@@ -7,6 +7,7 @@
 
 import Blackbird
 import SwiftUI
+import UIKit
 
 struct GameView: View {
     
@@ -14,81 +15,88 @@ struct GameView: View {
     @State var currentNodeId: Int = 1
     @State private var showTextMenu = false
     @State private var showMenu = false
-    @State private var buttonSwitch2 = false
+    @State private var buttonSwitch2 = true
     @AppStorage("isDarkMode") private var isDarkMode:Bool = false
     @Environment(\.presentationMode) var presentationMode
     
     // MARK: Computed properties
     var body: some View {
-        VStack(spacing: 10) {
-            
-            HStack {
-                Text("\(currentNodeId)")
-                    .font(.largeTitle)
+        ZStack(alignment: .bottom){
+            VStack(spacing: 10) {
+                
+                HStack {
+                    Text("\(currentNodeId)")
+                        .font(.largeTitle)
+                    Spacer()
+                }
+                
+                NodeView(currentNodeId: currentNodeId)
+                Divider()
+                
+                EdgesView(currentNodeId: $currentNodeId)
                 Spacer()
+                
+                HStack{
+                    Spacer()
+                    
+                    tabIcon1(showTextMenu: $showTextMenu, buttonSwitch2: $buttonSwitch2)
+                        .onTapGesture {
+                            withAnimation{
+                                showTextMenu.toggle()
+                                buttonSwitch2.toggle()
+                            }
+                        }
+                    //                Button(action: {}, label: {
+                    //                    Image(systemName: "textformat")
+                    //                        .resizable()
+                    //                        .aspectRatio(contentMode: .fill)
+                    //                        .frame(width: 30,height: 20)
+                    //                        .foregroundColor(.black)
+                    //                })
+                    
+                    
+                    Spacer()
+                    tabMenuIcon(showMenu: $showMenu, buttonSwitch2: $buttonSwitch2)
+                        .onTapGesture {
+                            withAnimation{
+                                buttonSwitch2.toggle()
+                                showMenu.toggle()
+                            }
+                        }
+                    //                Button(action: {}, label: {
+                    //                    Image(systemName: "light.max")
+                    //                        .resizable()
+                    //                        .aspectRatio(contentMode: .fill)
+                    //                        .frame(width: 30,height: 20)
+                    //                        .foregroundColor(.black)
+                    //                })
+                    Spacer()
+                    tabIcon2(buttonSwitch2: $buttonSwitch2)
+                        .onTapGesture {
+                            withAnimation{
+                                isDarkMode.toggle()
+                                buttonSwitch2.toggle()
+                            }
+                        }
+                    Spacer()
+                    //                Button(action: {
+                    //                    isDarkMode.toggle()
+                    //                }, label: {
+                    //                    Image(systemName: "moon.zzz")
+                    //                        .resizable()
+                    //                        .aspectRatio(contentMode: .fill)
+                    //                        .frame(width: 30,height: 20)
+                    //                        .foregroundColor(.black)
+                    //                })
+                }
+                .frame(height: UIScreen.main.bounds.height / 10)
+                .frame(width: UIScreen.main.bounds.width / 1)
+                .background(Color(.systemGray5))
             }
-            
-            NodeView(currentNodeId: currentNodeId)
-            Divider()
-            
-            EdgesView(currentNodeId: $currentNodeId)
-            Spacer()
-            
-            HStack{
-                Spacer()
-                
-                tabIcon1(showTextMenu: $showTextMenu)
-                    .onTapGesture {
-                        withAnimation{
-                            showTextMenu.toggle()
-                        }
-                    }
-//                Button(action: {}, label: {
-//                    Image(systemName: "textformat")
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fill)
-//                        .frame(width: 30,height: 20)
-//                        .foregroundColor(.black)
-//                })
-                
-                
-                Spacer()
-                tabMenuIcon(showMenu: $showMenu)
-                    .onTapGesture {
-                        withAnimation{
-                            showMenu.toggle()
-                        }
-                    }
-//                Button(action: {}, label: {
-//                    Image(systemName: "light.max")
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fill)
-//                        .frame(width: 30,height: 20)
-//                        .foregroundColor(.black)
-//                })
-                Spacer()
-                tabIcon2(buttonSwitch2: $buttonSwitch2)
-                    .onTapGesture {
-                        withAnimation{
-                            isDarkMode.toggle()
-                            buttonSwitch2.toggle()
-                        }
-                    }
-                Spacer()
-//                Button(action: {
-//                    isDarkMode.toggle()
-//                }, label: {
-//                    Image(systemName: "moon.zzz")
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fill)
-//                        .frame(width: 30,height: 20)
-//                        .foregroundColor(.black)
-//                })
+            if showMenu {
+                PopUpMenu()
+                .padding(.bottom, 120)
             }
-            .frame(height: UIScreen.main.bounds.height / 10)
-            .frame(width: UIScreen.main.bounds.width / 1)
-            .background(Color(.systemGray5))
-            
         }
         .padding()
         .ignoresSafeArea()
@@ -108,33 +116,50 @@ struct GameView_Previews: PreviewProvider {
 
 struct tabMenuIcon:View{
     @Binding var showMenu: Bool
+    @Binding var buttonSwitch2: Bool
     var body: some View{
         Image(systemName: "light.max")
             .resizable()
             .aspectRatio(contentMode: .fill)
             .frame(width: 30,height: 20)
-            .foregroundColor(.black)
+            .foregroundColor(buttonSwitch2 ? .black : .white)
     }
 }
 
 struct tabIcon1:View{
     @Binding var showTextMenu: Bool
+    @Binding var buttonSwitch2: Bool
     var body: some View{
         Image(systemName: "textformat")
             .resizable()
             .aspectRatio(contentMode: .fill)
             .frame(width: 30,height: 20)
-            .foregroundColor(.black)
+            .foregroundColor(buttonSwitch2 ? .black : .white)
     }
 }
 
 struct tabIcon2:View{
     @Binding var buttonSwitch2: Bool
     var body: some View{
-        Image(systemName: buttonSwitch2 ? "sun.max":"moon.zzz")
+        Image(systemName: buttonSwitch2 ? "moon.zzz" : "sun.max")
             .resizable()
             .aspectRatio(contentMode: .fill)
             .frame(width: 30,height: 20)
             .foregroundColor(buttonSwitch2 ? .black : .white)
+    }
+}
+
+struct PopUpMenu: View{
+    @State private var brightness: Double = 1.0
+    var body: some View{
+        HStack{
+            Text("Brightness:")
+            Slider(value: $brightness, in: 0.0...1.0){
+//                UIScreen.main.brightness = CGFloat(brightness)
+            }
+        }
+        .transition(.scale)
+        .padding(.horizontal,40)
+
     }
 }
